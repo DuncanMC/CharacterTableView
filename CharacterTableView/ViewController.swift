51d66411c05d1c8c546f97e55de7e3942b893d2d
Duncan Champney <DChampney@wareto.com>
2018-10-17 19:37:08 -0400

    Initial Commit

---
+++ CharacterTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CharacterTableView
+//
+//  Created by Duncan Champney on 10/17/18.
+//  Copyright © 2018 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
